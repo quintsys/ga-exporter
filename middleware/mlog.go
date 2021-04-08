b7fd28e122e60668577034b7f9c7c4bc4aa68a17
Erich Quintero <qbantek@gmail.com>
2021-04-08 13:30:39 -0400

    Add HTTP method to logs

--- middleware/mlog.go
+++ middleware/mlog.go
@@ -8,7 +8,7 @@ import (
 // LogWrap adds a logger around handlers
 func LogWrap(f func(w http.ResponseWriter, r *http.Request)) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("path: %s", r.URL.Path)
+		log.Printf("%s %s", r.Method, r.URL.Path)
 
 		f(w, r)
 	})
